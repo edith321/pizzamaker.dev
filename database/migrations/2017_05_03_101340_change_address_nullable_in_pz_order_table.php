846dd88e335a011a309d9aa25e4447ad2643c707
Edita <edita.paulikaite@inbox.lt>
2017-05-03 13:29:17 +0300

    Changed address to nullable

---
+++ database/migrations/2017_05_03_101340_change_address_nullable_in_pz_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeAddressNullableInPzOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pz_order', function (Blueprint $table) {
+
+            $table->string('address')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pz_order', function (Blueprint $table) {
+            //
+            $table->string('address')->nullable(false)->change();
+        });
+    }
+}
